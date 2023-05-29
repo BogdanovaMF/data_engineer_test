ffe1181b23cc9e7acfffd92f3d8b7d66bcd239cb
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-05-29 09:57:10 +0300

    fix errors

--- save_data_to_core/from_stage_to_core.py
+++ save_data_to_core/from_stage_to_core.py
@@ -1,22 +1,23 @@
 from utils.logger import get_logger
 from utils.mysql import mysql_connect
 
-conn = mysql_connect()
-cursor = conn.cursor()
-
+# conn = mysql_connect()
+# cursor = conn.cursor()
+#
 logger = get_logger()
 
 
-def insert_to_core(table_name_to: str, table_name_from):
+def insert_to_core(table_name_to: str, table_name_from, columns: str) -> None:
     """Inserting data into core from staging
     :param table_name_to: table names to retrieve data
     :param table_name_from: staging table
+    :param columns: columns table
     """
     try:
         query = f"""
             INSERT INTO {table_name_to} 
-                (exchange_date, source_id, currency_source_id, currency_destination_id, exchange_rate, load_rout_timestamp)
-            SELECT exchange_date,source_id, currency_source_id, currency_destination_id, extrange_rate, load_rout_timestamp
+                ({columns})
+            SELECT {columns}
             FROM {table_name_from};
             """
         cursor.execute(query)
@@ -28,16 +29,14 @@ def insert_to_core(table_name_to: str, table_name_from):
         raise ex
 
 
-def update_core(table_name: str):
+def update_core(table_name: str, params_query: str):
     """Updating data into core from staging
     :param table_name: table names to retrieve data
+    param: params_query: query's parameters
     """
     try:
         queries = f"""
-            UPDATE {table_name} SET language_id=1 WHERE currency_source_id='USD';
-            UPDATE {table_name} SET language_id=2 WHERE currency_source_id='RUB';
-            UPDATE {table_name} SET language_id=4 WHERE currency_source_id='EUR';
-            UPDATE {table_name} SET language_id=3 WHERE currency_source_id='CNY';
+            UPDATE {table_name} {params_query};
         """
         cursor.execute(queries)
         conn.commit()
@@ -49,6 +48,15 @@ def update_core(table_name: str):
 
 
 if __name__ == '__main__':
-    insert_to_core('stage_currencies', 'exchange_rates')
-    update_core('exchange_rates')
+    columns = 'exchange_date,source_id, currency_source_id, currency_destination_id, extrange_rate, load_rout_timestamp'
+    insert_to_core('stage_currencies', 'exchange_rates', columns)
+
+    params_query1 = "SET language_id=1 WHERE currency_source_id='USD'"
+    params_query2 = "SET language_id=2 WHERE currency_source_id='RUB'"
+    params_query3 = "SET language_id=4 WHERE currency_source_id='EUR'"
+    params_query4 = "SET language_id=3 WHERE currency_source_id='CNY'"
+    update_core('exchange_rates', params_query1)
+    update_core('exchange_rates', params_query2)
+    update_core('exchange_rates', params_query3)
+    update_core('exchange_rates', params_query4)
 
