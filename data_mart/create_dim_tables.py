8507dda5b6ed9327840735a100d3fa965d8170f5
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-05-29 09:36:33 +0300

    fix errors

--- data_mart/create_dim_tables.py
+++ data_mart/create_dim_tables.py
@@ -25,29 +25,27 @@ def create_table_dim(table_name: str, params_query: str):
         raise ex
 
 
-param_dim_currencies = f"""
-        SELECT 
-        exchange_date, language_id, currency_source_id, currency_destination_id, exchange_rate, load_rout_timestamp, source_id
-        FROM exchange_rates
-    """
-
-param_dim_currencies_rus = """
-        SELECT 
-        exchange_date, curr_name_rus, currency_source_id, currency_destination_id, exchange_rate, load_rout_timestamp, source_id
-        FROM exchange_rates AS t1
-        INNER JOIN languages as t2 
-        ON t1.language_id=t2.language_id"""
-
-
-param_dim_currencies_eng = """                                                                                                  
-        SELECT                                                                                                                  
-        exchange_date, curr_name_eng, currency_source_id, currency_destination_id, exchange_rate, load_rout_timestamp, source_id
-        FROM exchange_rates AS t1                                                                                               
-        INNER JOIN languages as t2                                                                                              
-        ON t1.language_id=t2.language_id
+if __name__ == "__main__":
+    param_dim_currencies = f"""
+            SELECT 
+            exchange_date, language_id, currency_source_id, currency_destination_id, exchange_rate, load_rout_timestamp, source_id
+            FROM exchange_rates
         """
 
-if __name__ == "__main__":
+    param_dim_currencies_rus = """
+            SELECT 
+            exchange_date, curr_name_rus, currency_source_id, currency_destination_id, exchange_rate, load_rout_timestamp, source_id
+            FROM exchange_rates AS t1
+            INNER JOIN languages as t2 
+            ON t1.language_id=t2.language_id"""
+
+    param_dim_currencies_eng = """                                                                                                  
+            SELECT                                                                                                                  
+            exchange_date, curr_name_eng, currency_source_id, currency_destination_id, exchange_rate, load_rout_timestamp, source_id
+            FROM exchange_rates AS t1                                                                                               
+            INNER JOIN languages as t2                                                                                              
+            ON t1.language_id=t2.language_id
+            """
     create_table_dim('dim_currencies', param_dim_currencies)
     create_table_dim('dim_currencies_rus', param_dim_currencies_rus)
     create_table_dim('dim_currencies_eng', param_dim_currencies_eng)
